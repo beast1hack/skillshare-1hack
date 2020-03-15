d5a597c71c185d9430e2c6308354ef19be2937f5
ashim chapagai <jdillon8@student.cccd.edu>
2020-03-15 14:57:54 +0545

    skillshare dl for 1hack

---
+++ README.md
@@ -0,0 +1,11 @@
+# Skillshare-DL
+
+<h4>📝 Note: Downloader core is not written by me. i found this python downloader from a  forum. i did few modification to code then implemented it into colab. all credit goes to original python script dev.</h4>
+
+
+## Disclaimer
+
+I will not be responsible for how you use Skillshare-DL
+Although the downloaded files are not the contents themselves, accessing or storing these files might still be illegal in some parts of the world. So, take great care when using this tool and make sure that it is legal.
+"# skillshare-dl" 
+"# skillshare-dl" 
---
+++ skillshare.py
@@ -0,0 +1,105 @@
+import requests, json, sys, re, os
+from slugify import slugify
+
+class Skillshare(object):
+
+    def __init__(
+        self,
+        cookie,
+        download_path=os.environ.get('FILE_PATH', './Skillshare'),
+        pk='BCpkADawqM2OOcM6njnM7hf9EaK6lIFlqiXB0iWjqGWUQjU7R8965xUvIQNqdQbnDTLz0IAO7E6Ir2rIbXJtFdzrGtitoee0n1XXRliD-RH9A-svuvNW9qgo3Bh34HEZjXjG4Nml4iyz3KqF',
+        brightcove_account_id=3695997568001,
+    ):
+        self.cookie = cookie.strip().strip('"')
+        self.download_path = download_path
+        self.pk = pk.strip()
+        self.brightcove_account_id = brightcove_account_id
+        self.pythonversion = 3 if sys.version_info >= (3, 0) else 2
+
+    def is_unicode_string(self, string):
+        if (self.pythonversion == 3 and isinstance(string, str)) or (self.pythonversion == 2 and isinstance(string, unicode)):
+            return True
+
+        else:
+            return False
+
+    def download_course_by_url(self, url):
+        m = re.match('https://www.skillshare.com/classes/.*?/(\\d+)', url)
+        assert m, 'Failed to parse class ID from URL'
+        self.download_course_by_class_id(m.group(1))
+
+    def download_course_by_class_id(self, class_id):
+        data = self.fetch_course_data_by_class_id(class_id=class_id)
+        teacher_name = None
+        if 'vanity_username' in data['_embedded']['teacher']:
+            teacher_name = data['_embedded']['teacher']['vanity_username']
+        if not teacher_name:
+            teacher_name = data['_embedded']['teacher']['full_name']
+        assert teacher_name, 'Failed to read teacher name from data'
+        if self.is_unicode_string(teacher_name):
+            teacher_name = teacher_name.encode('ascii', 'replace')
+        title = data['title']
+        if self.is_unicode_string(title):
+            title = title.encode('ascii', 'replace')
+        base_path = os.path.abspath(os.path.join(self.download_path, slugify(teacher_name), slugify(title))).rstrip('/')
+        if not os.path.exists(base_path):
+            os.makedirs(base_path)
+        for u in data['_embedded']['units']['_embedded']['units']:
+            for s in u['_embedded']['sessions']['_embedded']['sessions']:
+                video_id = None
+                if 'video_hashed_id' in s:
+                    if s['video_hashed_id']:
+                        video_id = s['video_hashed_id'].split(':')[1]
+                    assert video_id, 'Failed to read video ID from data'
+                    s_title = s['title']
+                    if self.is_unicode_string(s_title):
+                        s_title = s_title.encode('ascii', 'replace')
+                    file_name = '{} - {}'.format(str(s['index'] + 1).zfill(2), slugify(s_title))
+                    self.download_video(fpath='{base_path}/{session}.mp4'.format(base_path=base_path,
+                      session=file_name),
+                      video_id=video_id)
+                    print('')
+
+    def fetch_course_data_by_class_id(self, class_id):
+        res = requests.get(url=('https://api.skillshare.com/classes/{}'.format(class_id)),
+          headers={'Accept':'application/vnd.skillshare.class+json;,version=0.8',
+         'User-Agent':'Skillshare/4.1.1; Android 5.1.1',
+         'Host':'api.skillshare.com',
+         'cookie':self.cookie})
+        assert res.status_code == 200, 'Fetch error, code == {}'.format(res.status_code)
+        return res.json()
+
+    def download_video(self, fpath, video_id):
+        meta_url = 'https://edge.api.brightcove.com/playback/v1/accounts/{account_id}/videos/{video_id}'.format(account_id=(self.brightcove_account_id),
+          video_id=video_id)
+        meta_res = requests.get(meta_url,
+          headers={'Accept':'application/json;pk={}'.format(self.pk),
+         'User-Agent':'Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0',
+         'Origin':'https://www.skillshare.com'})
+        assert not meta_res.status_code != 200, 'Failed to fetch video meta'
+        for x in meta_res.json()['sources']:
+            if 'container' in x:
+                if x['container'] == 'MP4' and 'src' in x:
+                    dl_url = x['src']
+                    break
+
+        print('Downloading {}...'.format(fpath))
+        if os.path.exists(fpath):
+            print('Video already downloaded, skipping...')
+            return
+        with open(fpath, 'wb') as (f):
+            response = requests.get(dl_url, allow_redirects=True, stream=True)
+            total_length = response.headers.get('content-length')
+            if not total_length:
+                f.write(response.content)
+            else:
+                dl = 0
+                total_length = int(total_length)
+                for data in response.iter_content(chunk_size=4096):
+                    dl += len(data)
+                    f.write(data)
+                    done = int(50 * dl / total_length)
+                    sys.stdout.write('\r[%s%s]' % ('=' * done, ' ' * (50 - done)))
+                    sys.stdout.flush()
+
+            print('')
